b0e06d03dddcf97f5ba13080e28c2599e4c4ca47
charsov <charsov.e@numdes.com>
2021-07-27 11:40:00 +0300

    find unsorted subarray

---
+++ find_unsorted_subarray.py
@@ -0,0 +1,49 @@
+import random
+
+
+def find_unsorted_subarray(array):
+    for i in range(1, len(array)):
+        if array[i] < array[i - 1]:
+            start_interval = i - 1
+            break
+    else:
+        return [-1, -1]
+
+    for i in range(start_interval + 1, len(array)):
+        if array[i] >= array[start_interval]:
+            end_interval = i - 1
+            break
+    else:
+        end_interval = len(array)
+
+    return [start_interval, end_interval]
+
+
+def test_find_unsorted_subarray_simple_case():
+    array = [1, 4, 3, 2, 3, 4]
+    result = find_unsorted_subarray(array)
+    assert result == [1, 4]
+
+
+def test_find_unsorted_subarray_in_sorted_array():
+    array = sorted([random.randrange(100) for _ in range(random.randrange(100))])
+    result = find_unsorted_subarray(array)
+    assert result == [-1, -1]
+
+
+def test_find_unsorted_subarray_in_empty_array():
+    array = []
+    result = find_unsorted_subarray(array)
+    assert result == [-1, -1]
+
+
+def test_find_unsorted_subarray_in_array_with_one_value():
+    array = [random.randrange(100), ]
+    result = find_unsorted_subarray(array)
+    assert result == [-1, -1]
+
+
+def test_find_unsorted_subarray_at_the_end_of_array():
+    array = [1, 2, 5, 3]
+    result = find_unsorted_subarray(array)
+    assert result == [2, 4]
