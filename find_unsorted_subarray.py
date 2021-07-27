959bdc46925ee3f83d4ba86b0bf1a6567f5f5fff
charsov <charsov.e@numdes.com>
2021-07-27 11:54:40 +0300

    refactor code

--- find_unsorted_subarray.py
+++ find_unsorted_subarray.py
@@ -7,7 +7,7 @@ def find_unsorted_subarray(array):
             start_interval = i - 1
             break
     else:
-        return [-1, -1]
+        return []
 
     for i in range(start_interval + 1, len(array)):
         if array[i] >= array[start_interval]:
@@ -21,29 +21,29 @@ def find_unsorted_subarray(array):
 
 def test_find_unsorted_subarray_simple_case():
     array = [1, 4, 3, 2, 3, 4]
-    result = find_unsorted_subarray(array)
-    assert result == [1, 4]
+    interval = find_unsorted_subarray(array)
+    assert interval == [1, 4]
 
 
 def test_find_unsorted_subarray_in_sorted_array():
     array = sorted([random.randrange(100) for _ in range(random.randrange(100))])
-    result = find_unsorted_subarray(array)
-    assert result == [-1, -1]
+    interval = find_unsorted_subarray(array)
+    assert interval == []
 
 
 def test_find_unsorted_subarray_in_empty_array():
     array = []
-    result = find_unsorted_subarray(array)
-    assert result == [-1, -1]
+    interval = find_unsorted_subarray(array)
+    assert interval == []
 
 
 def test_find_unsorted_subarray_in_array_with_one_value():
     array = [random.randrange(100), ]
-    result = find_unsorted_subarray(array)
-    assert result == [-1, -1]
+    interval = find_unsorted_subarray(array)
+    assert interval == []
 
 
 def test_find_unsorted_subarray_at_the_end_of_array():
     array = [1, 2, 5, 3]
-    result = find_unsorted_subarray(array)
-    assert result == [2, 3]
+    interval = find_unsorted_subarray(array)
+    assert interval == [2, 3]
